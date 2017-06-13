4316b1fbb1144890f24fe39573774aa07859b9d8
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-06-13 19:01:35 -0300

    melhoria de legibilidade para fabrica. Criacao de metodos para crear diferentes categorias

--- PokerHand/PokerHandJava/src/main/java/alex/treinamento/model/handcard/HandFactory.java
+++ PokerHand/PokerHandJava/src/main/java/alex/treinamento/model/handcard/HandFactory.java
@@ -14,37 +14,56 @@ public class HandFactory {
     public HandCard build(HandSpecification specification){
 
         if (specification.hasSequence()){
-            Sequence sequence = specification.getSequence();
-            if (sequence.isAllSameSuit()){
-                if (sequence.getInitValue().isSameThan(CardValue.TEN)){
-                    return HandType.ROYAL_FLUSH.getHandCard(specification);
-                }
+            return createSequenceHand(specification);
+        }
 
-                return HandType.STRAIGHT_FLUSH.getHandCard(specification);
-            }
+        if (specification.hasGroup()){
+            return createGroupHand(specification);
+        }
 
-            return HandType.SEQUENCE.getHandCard(specification);
-        } else if (specification.hasGroup()){
-            Group group = specification.getGroup();
-            if (group.getLength() == FourKindHand.GROUP_LENGTH){
-                return HandType.FOUR_KIND.getHandCard(specification);
-            } else if (group.isValid()){
-                if (specification.hasPairs()){
-                    return HandType.FULL_HOUSE.getHandCard(specification);
-                }
-                return HandType.THREE_KIND.getHandCard(specification);
-            }
-        } else if (specification.hasPairs()){
-            List<Pair> pairs = specification.getPairs();
-            if (pairs.size() == TwoPairHand.PAIR_LENGTH){
-                return HandType.TWO_PAIR.getHandCard(specification);
-            }
+        if (specification.hasPairs()){
+            return createPairHand(specification);
+        }
 
-            return HandType.ONE_PAIR.getHandCard(specification);
-        } else if (specification.isAllCardsSameSuit()){
+        if (specification.isAllCardsSameSuit()){
             return HandType.FLUSH.getHandCard(specification);
         }
 
         return HandType.HIGH_CARD.getHandCard(specification);
+    }
+
+    private HandCard createGroupHand(HandSpecification specification) {
+        Group group = specification.getGroup();
+        if (group.getLength() == FourKindHand.GROUP_LENGTH){
+            return HandType.FOUR_KIND.getHandCard(specification);
+        } else if (group.isValid()){
+            if (specification.hasPairs()){
+                return HandType.FULL_HOUSE.getHandCard(specification);
+            }
+            return HandType.THREE_KIND.getHandCard(specification);
+        }
+        return null;
+    }
+
+    private HandCard createPairHand(HandSpecification specification) {
+        List<Pair> pairs = specification.getPairs();
+        if (pairs.size() == TwoPairHand.PAIR_LENGTH){
+            return HandType.TWO_PAIR.getHandCard(specification);
+        }
+
+        return HandType.ONE_PAIR.getHandCard(specification);
+    }
+
+    private HandCard createSequenceHand(HandSpecification specification) {
+        Sequence sequence = specification.getSequence();
+        if (sequence.isAllSameSuit()){
+            if (sequence.getInitValue().isSameThan(CardValue.TEN)){
+                return HandType.ROYAL_FLUSH.getHandCard(specification);
+            }
+
+            return HandType.STRAIGHT_FLUSH.getHandCard(specification);
+        }
+
+        return HandType.SEQUENCE.getHandCard(specification);
     }
 }
