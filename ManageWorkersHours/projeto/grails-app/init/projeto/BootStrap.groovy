011ec7d25c42f52aa00e5e169ed4138250802249
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-06-23 09:31:24 -0300

    add adicao de adm no bootStrap

--- ManageWorkersHours/projeto/grails-app/init/projeto/BootStrap.groovy
+++ ManageWorkersHours/projeto/grails-app/init/projeto/BootStrap.groovy
@@ -1,8 +1,12 @@
 package projeto
 
+import user.Manager
+import user.User
+
 class BootStrap {
 
     def init = { servletContext ->
+        new Manager(name: "Adm", userEmail: "arf92@live.com", password: "adm123", department: "Administração").save()
     }
     def destroy = {
     }
