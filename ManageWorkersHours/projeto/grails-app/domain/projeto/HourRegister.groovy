6b6d64d04b7c870a2f27540a1950c5f092b303b9
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-06-23 17:17:19 -0300

    add de mapping para enum e construtor padrao

--- ManageWorkersHours/projeto/grails-app/domain/projeto/HourRegister.groovy
+++ ManageWorkersHours/projeto/grails-app/domain/projeto/HourRegister.groovy
@@ -6,17 +6,37 @@ import user.Employer
 import user.Manager
 import user.User
 
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+
 @EqualsAndHashCode
 class HourRegister {
 
     Employer employer
     Manager manager
 
+    @Enumerated(EnumType.ORDINAL)
     HourRegisterStatus status
     Date manageTime
     String reason
+    Date dateCreated
+    Date lastUpdate
+
+    HourRegister(Employer employer, Date dateCreated) {
+        this.employer = employer
+        status = HourRegisterStatus.REQUESTED
+        this.dateCreated = dateCreated
+    }
+    static mapping = {
+        status enumType:"ordinal"
+    }
 
     static constraints = {
+        manageTime nullable: true
+        manager  nullable: true
+        employer nullable: false
+        lastUpdate nullable: true
+        reason nullable: true
     }
 
 }
