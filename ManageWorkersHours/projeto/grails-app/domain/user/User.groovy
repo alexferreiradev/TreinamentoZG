6eead75d2f63e0b3acc6fcfeb3ddc800cbe9528a
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-06-23 08:56:27 -0300

    add User domain como super classe de user

--- ManageWorkersHours/projeto/grails-app/domain/user/User.groovy
+++ ManageWorkersHours/projeto/grails-app/domain/user/User.groovy
@@ -5,8 +5,9 @@ import groovy.transform.EqualsAndHashCode
 @EqualsAndHashCode
 class User {
 
-    public static final int ADM_PERMISSION = 0
-    public static final int EMPLOYER_PERMISSION = 1
+    static mapping = {
+        tablePerHierarchy false
+    }
 
     String name
     String lastName
@@ -14,9 +15,6 @@ class User {
     String userEmail
     String password
     String address
-    String job
-    int permission
-    float salary
 
     static constraints = {
     }
