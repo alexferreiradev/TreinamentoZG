ccbf6da000a391491cd04fcc35c76d97aaf67904
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-06-23 17:17:58 -0300

    add criacao de funcionario teste

--- ManageWorkersHours/projeto/grails-app/init/projeto/BootStrap.groovy
+++ ManageWorkersHours/projeto/grails-app/init/projeto/BootStrap.groovy
@@ -1,5 +1,6 @@
 package projeto
 
+import user.Employer
 import user.Manager
 import user.User
 
@@ -7,7 +8,9 @@ class BootStrap {
 
     def init = { servletContext ->
         new Manager(name: "Adm", userEmail: "arf92@live.com", password: "adm123", department: "Administração").save()
+        new Employer(name: "TesteFunc", lastName: "LastName", userEmail: "arf92@live.com", password: "123", workHours: "8", salary: "3000", job: "Desenvolvedor" ).save()
     }
+
     def destroy = {
     }
 }
