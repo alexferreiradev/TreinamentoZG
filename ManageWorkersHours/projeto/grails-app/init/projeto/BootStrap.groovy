a12ae7e409d356a287def373e47692a3925845f0
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-06-30 13:18:59 -0300

    uso de enum de role

--- ManageWorkersHours/projeto/grails-app/init/projeto/BootStrap.groovy
+++ ManageWorkersHours/projeto/grails-app/init/projeto/BootStrap.groovy
@@ -1,16 +1,14 @@
 package projeto
 
-import grails.plugin.springsecurity.SpringSecurityService
 import user.Manager
 import user.RoleType
 import user.SecRole
-import user.SecUser
 import user.SecUserSecRole
 
 class BootStrap {
 
     def init = { servletContext ->
-        SecRole.findByAuthority(RoleType.ROLE_USER) ?: new SecRole(authority: RoleType.ROLE_USER).save(failOnError: true)
+        SecRole.findByAuthority(RoleType.ROLE_EMPYEE) ?: new SecRole(authority: RoleType.ROLE_EMPYEE).save(failOnError: true)
         SecRole adminRole = SecRole.findByAuthority(RoleType.ROLE_ADM) ?: new SecRole(authority: RoleType.ROLE_ADM).save(failOnError: true)
 
         def adminUser = Manager.findByUsername('admin') ?: new Manager(
