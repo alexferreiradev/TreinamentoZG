b629f2100f7a7974aae62c3badfb0de2f32fe442
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-06-30 11:25:24 -0300

    add enum para role

--- ManageWorkersHours/projeto/grails-app/init/projeto/BootStrap.groovy
+++ ManageWorkersHours/projeto/grails-app/init/projeto/BootStrap.groovy
@@ -2,35 +2,37 @@ package projeto
 
 import grails.plugin.springsecurity.SpringSecurityService
 import user.Manager
+import user.RoleType
 import user.SecRole
 import user.SecUser
 import user.SecUserSecRole
 
 class BootStrap {
 
-    SpringSecurityService springSecurityService
-
     def init = { servletContext ->
-        def userRole = SecRole.findByAuthority('ROLE_USER') ?: new SecRole(authority: 'ROLE_USER').save(failOnError: true)
-        def adminRole = SecRole.findByAuthority('ROLE_ADMIN') ?: new SecRole(authority: 'ROLE_ADMIN').save(failOnError: true)
+        SecRole.findByAuthority(RoleType.ROLE_USER) ?: new SecRole(authority: RoleType.ROLE_USER).save(failOnError: true)
+        SecRole adminRole = SecRole.findByAuthority(RoleType.ROLE_ADM) ?: new SecRole(authority: RoleType.ROLE_ADM).save(failOnError: true)
 
-        def adminUser = SecUser.findByUsername('admin') ?: new SecUser(
+        def adminUser = Manager.findByUsername('admin') ?: new Manager(
                 username: 'admin',
                 password: 'admin',
                 enabled: true,
+                name: "ADM",
+                userEmail: "arf92liv.omc",
+                enterprise: "ZG solucoes",
         )
 
         adminUser.validate()
         adminUser.save(failOnError: true)
-        Manager manager = new Manager(
-                name: "ADM",
-                userEmail: "arf92liv.omc",
-                username: 'admin',
-                password: 'admin',
-                enabled: true,
-                enterprise: "ZG solucoes"
-        )
-        manager.save()
+//        Manager manager = new Manager(
+//                name: "ADM",
+//                userEmail: "arf92liv.omc",
+//                username: 'admin',
+//                password: 'admin',
+//                enabled: true,
+//                enterprise: "ZG solucoes"
+//        )
+//        manager.save()
 
         if (!adminUser.authorities.contains(adminRole)) {
             SecUserSecRole.create adminUser, adminRole
---
+++ ManageWorkersHours/projeto/src/main/groovy/user/RoleType.groovy
@@ -0,0 +1,11 @@
+package user
+
+/**
+ * Created by alexferreira on 30/06/17.
+ */
+enum RoleType {
+
+    ROLE_USER,
+    ROLE_ADM,
+
+}
