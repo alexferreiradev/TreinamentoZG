00c07febc86a325b580c280c8841cd95fc2f584a
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-06-30 11:19:32 -0300

    tornando a classe SecUser abstrata

--- ManageWorkersHours/projeto/grails-app/domain/user/SecUser.groovy
+++ ManageWorkersHours/projeto/grails-app/domain/user/SecUser.groovy
@@ -7,7 +7,7 @@ import grails.compiler.GrailsCompileStatic
 @GrailsCompileStatic
 @EqualsAndHashCode(includes='username')
 @ToString(includes='username', includeNames=true, includePackage=false)
-class SecUser implements Serializable {
+abstract class SecUser implements Serializable {
 
     private static final long serialVersionUID = 1
 
