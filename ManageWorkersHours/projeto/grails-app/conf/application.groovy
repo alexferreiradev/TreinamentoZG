4f1d8dca1d26c05735dc1e3be6fca6afc471418d
Alex Ferreira <alexferreira@zgsolucoes.com.br>
2017-06-30 13:18:45 -0300

    add role com enum e remocao de filtros

--- ManageWorkersHours/projeto/grails-app/conf/application.groovy
+++ ManageWorkersHours/projeto/grails-app/conf/application.groovy
@@ -1,7 +1,12 @@
 // Added by the Spring Security Core plugin:
-grails.plugin.springsecurity.userLookup.userDomainClassName = 'user.User'
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'user.SecUser'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'user.SecUserSecRole'
 grails.plugin.springsecurity.authority.className = 'user.SecRole'
+grails.plugin.springsecurity.rememberMe.persistentToken.domainClassName = 'user.PersistentLogin'
+grails.plugin.springsecurity.rememberMe.persistent = true
+
+
+// Regras de acesso
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/',               access: ['permitAll']],
 	[pattern: '/error',          access: ['permitAll']],
@@ -15,6 +20,14 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/**/favicon.ico', access: ['permitAll']]
 ]
 
+// Filtros
+//grails.plugin.springsecurity.filterChain.filterNames = [
+//		'securityContextPersistenceFilter', 'logoutFilter',
+//		'authenticationProcessingFilter', 'rememberMeAuthenticationFilter',
+//		'anonymousAuthenticationFilter', 'exceptionTranslationFilter',
+//		'filterInvocationInterceptor',
+//]
+
 grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/assets/**',      filters: 'none'],
 	[pattern: '/**/js/**',       filters: 'none'],
@@ -24,12 +37,10 @@ grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/**',             filters: 'JOINED_FILTERS']
 ]
 
-grails.plugin.springsecurity.securityConfigType = SecurityConfigType.Annotation
+grails.plugin.springsecurity.securityConfigType = SecurityConfigType.InterceptUrlMap
 grails.plugin.springsecurity.interceptUrlMap = [
-        '/employer/**':         ['ROLE_USER'],
-        '/manager/**':         ['ROLE_ADMIN'],
-        '/**':               ['IS_AUTHENTICATED_ANONYMOUSLY'],
+		'/manager/index':         [RoleType.ROLE_ADM],
+		'/employer/**':         [RoleType.ROLE_EMPYEE, RoleType.ROLE_ADM],
+		'/**':               ['IS_AUTHENTICATED_ANONYMOUSLY'],
 ]
 
-grails.plugin.springsecurity.rememberMe.persistent = true
-grails.plugin.springsecurity.rememberMe.persistentToken.domainClassName = 'user.PersistentLogin'
